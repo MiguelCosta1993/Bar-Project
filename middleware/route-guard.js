8030971ddd0c9c377c1c7be5b7e9511d19a6f1ea
goncalofm90 <goncalofm90@gmail.com>
2020-08-06 17:22:51 +0100

    finishes styles

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -7,8 +7,8 @@ module.exports = (req, res, next) => {
   if (req.user) {
     next();
   } else {
-    const error = new Error('AUTHENTICATION_REQUIRED');
-    error.status = 401;
+    const error = new Error('You need to sign in to do that.');
+    // error.status = 401;
     next(error);
   }
 };
