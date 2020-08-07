b407a1c660544066bc5181310da8b673208b9760
Miguel Costa <miguelcosta1993@gmail.com>
2020-08-07 10:59:10 +0100

    edit email to do

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const User = require('./../models/user');
 const routeGuard = require('./../middleware/route-guard');
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+const multerStorageCloudinary = require('multer-storage-cloudinary');
+
+const storage = new multerStorageCloudinary.CloudinaryStorage({
+  cloudinary: cloudinary.v2
+});
+const upload = multer({ storage });
 
 const profileRouter = new express.Router();
 
@@ -12,17 +20,25 @@ profileRouter.get('/edit', routeGuard, (req, res, next) => {
   res.render('profile/edit');
 });
 
-profileRouter.post('/edit', routeGuard, (req, res, next) => {
-  const id = req.session.userId;
-  const { name, email, image, bio } = req.body;
+profileRouter.post(
+  '/edit',
+  routeGuard,
+  upload.single('image'),
 
-  User.findByIdAndUpdate(id, { name, email, image, bio })
-    .then(() => {
-      res.redirect('/profile/display');
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+  (req, res, next) => {
+    const id = req.session.user;
+    const { name, email, bio } = req.body;
+    const image = req.file.path;
+    console.log(id);
+
+    User.findByIdAndUpdate(id, { name, email, image, bio })
+      .then(() => {
+        res.redirect('/profile/display');
+      })
+      .catch(error => {
+        next(error);
+      });
+  }
+);
 
 module.exports = profileRouter;
